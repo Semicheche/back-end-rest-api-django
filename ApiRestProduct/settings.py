0f414d9f37955a3dac691c46f8edeac4c0499bac
Luciano Semicheche <lucianosemicheche@gmail.com>
2020-10-03 18:31:17 -0300

    [Add] .gitignore

--- ApiRestProduct/settings.py
+++ ApiRestProduct/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'ApiRestProduct.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'api_rest_product_db',
+        'NAME': 'test_product_db',
         'HOST': '127.0.0.1',
         'PORT': 27017,
     }
